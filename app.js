5ce8f67c93fb8ab6dfb242ef4e346e59e6a1ad84
Roman <pogrebniakroman@gmail.com>
2022-09-16 12:03:45 +0200

    firstNam

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const name = 'Roman';
 
-function passTest(result){
-    let firstName;
-    let age;
-if(result){
-    firstName = 'Roman';
-     age = 41;
-}
-console.log(firstName + 'birthday'+ age);
-}
-passTest(true);
+ let firstName = 'John';
+ let lastName = 'Snow';
+ let yearOfBirth = 1990;
+const str = `${firstName} ${lastName}`;
+console.log(str);
+console.log(str.startsWith('J'));
+console.log(str.includes('th'));
+
+
